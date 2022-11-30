a384c072552b227d01c52127b279418dd5a0b21b
Stephane Paul <stephane@remediumdev.com>
2022-11-29 19:24:59 -0500

    chore: initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2022-11-29
+
+- Initial release
---
+++ lib/anonym.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "anonym/version"
+
+module Anonym
+  class Error < StandardError; end
+  # Your code goes here...
+end
